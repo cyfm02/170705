211088c5cf2f434786d7308888c2e900655833f5
Wolfgang Chien <sakalabahonda@gmail.com>
2017-07-03 15:07:49 +0000

    add 110_TryCath, lean 100_OOP

--- 110_TryCatch/Demo_Catch0.php
+++ 110_TryCatch/Demo_Catch0.php
@@ -2,11 +2,11 @@
 
 $x = 3;
 $y = 0;
-echo "<br>test Flag 1";
+echo "test Flag 1<hr>";
 $result = $x / $y; // Warning and go ahead.
-echo "<br>test Flag 2";
+echo "test Flag 2<hr>";
 throw new Exception("Error!!");  // Fatal error and program stop
-echo "<br>test Flag 3";
+echo "test Flag 3<hr>";
 
 ?>
 
--- 110_TryCatch/Demo_Catch1.php
+++ 110_TryCatch/Demo_Catch1.php
@@ -4,17 +4,17 @@ $x = 3;
 $y = 0;
 try
 {
-	echo "<br>test Flag 1";
+	echo "test Flag 1<hr>";
 	$result = $x / $y; // Warning and go ahead.
-	echo "<br>test Flag 2";
+	echo "test Flag 2<hr>";
 	throw new Exception("Error!!");  // Fatal error and program stop
 }
 catch (Exception $err)
 {
-	echo "<br>trap into CATCH block.";
-	echo "<br>" . $err->getMessage();
+	echo "trap into CATCH block.<hr>";
+	echo $err->getMessage() . "<hr>";
 }
-echo "<br>test Flag 3";
+echo "test Flag 3";
 
 ?>
 
